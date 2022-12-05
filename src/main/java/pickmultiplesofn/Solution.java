b4738c4bb22e565378fea0075b31087aff21a309
HanJungkyu <jk891113@naver.com>
2022-12-05 18:41:30 +0900

    n의 배수 고르기

---
+++ src/main/java/pickmultiplesofn/Solution.java
@@ -0,0 +1,16 @@
+package pickmultiplesofn;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        List<Integer> arr = new ArrayList<>();
+        for (int num: numlist) {
+            if (num % n == 0) {
+                arr.add(num);
+            }
+        }
+        int[] answer = arr.stream().mapToInt(x -> x).toArray();
+        return answer;
+    }
+}
